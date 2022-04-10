c9d29ce88908bf77b1227b2697958722f3bbcbcc
selimcanermis <ermisselimcan@gmail.com>
2022-04-10 15:10:45 +0300

    main class finish

--- main.py
+++ main.py
@@ -48,8 +48,16 @@ driver.get(udemy_url)
 #time.sleep(10)
 
 
-def extract_text(soup_obj, tag, attribute_name, attribute_value):
-    txt = soup_obj.find(tag, {attribute_name: attribute_value}).text().strip() if soup_obj.find(tag, {attribute_name: attribute_value}) else ''
+def take_out_text(soup_obj, tag, attribute_name, attribute_value):
+    #txt = soup_obj.find(tag, {attribute_name: attribute_value}).text().strip() if soup_obj.find(tag, {attribute_name: attribute_value}) else ''
+    #print(tag)
+    #print(attribute_name)
+    #print(attribute_value)
+    
+    txt = soup_obj.find(tag, {attribute_name: attribute_value}).text().strip()
+    print(txt)
+    txt = "selim"
+    print(type(txt))
     return txt
 
 course_rows = []
@@ -70,21 +78,32 @@ for page_number in range(1,2):
         #courses = course_list.find_all('a', {'class': 'udlite-custom-focus-visible browse-course-card--link--3KIkQ'})
         courses = course_list.find_all('div', {'class': 'course-card--container--1QM2W course-card--large--2aYkn'})
         print("girdim else")
-        print(course_list)
-        print(courses)
+        #print(course_list)
+        #print(courses)
+        print(type(courses))
 
         for course in courses:
             print("girdim for")
             course_url = '{0}{1}'.format('https://www.udemy.com', course.find('a')['href'])
-            course_title = course.select('div[class*="course-card--course-title"]')[0].text
-            course_headline = extract_text(course, 'p', 'data-purpose', 'safely-set-inner-html:course-card:course-headline')            
-            author = extract_text(course, 'div', 'data-purpose', 'safely-set-inner-html:course-card:visible-instructors')
-            course_rating = extract_text(course, 'span', 'data-purpose', 'rating-number')
-            number_of_ratings = extract_text(course, 'span', 'class', 'udlite-text-xs course-card--reviews-text--12UpL')[1:-1]
-            course_detail = course.find_all('span', {'class':'course-card--row--1OMjg'})
+            print(course_url)
+            course_title = course.find('a').text
+            print(course_title)
+            #course_headline = take_out_text(course, 'p', 'class', 'udlite-text-sm course-card--course-headline--2DAqq')            
+            course_headline = soup.find("p",{"class": "udlite-text-sm course-card--course-headline--2DAqq"}).text.strip()
+            print(course_headline)
+            #author = take_out_text(course, 'div', 'class', 'course-card--instructor-list--nH1OC')
+            author = soup.find("div", {"class": "course-card--instructor-list--nH1OC"}).text.strip()
+            print(author)
+            #course_rating = take_out_text(course, 'span', 'data-purpose', 'rating-number')
+            course_rating = soup.find_all("span", {"class": "udlite-sr-only"})[1].text.strip()
+            print(course_rating)
+            #number_of_ratings = take_out_text(course, 'span', 'class', 'udlite-text-xs course-card--reviews-text--12UpL')[1:-1]
+            number_of_ratings = soup.find("span", {"class": "udlite-heading-sm star-rating--rating-number--2o8YM"}).text.strip()
+            print(number_of_ratings)
+            course_detail = course.find_all('div', {'class':'course-card--row--29Y0w'})
             course_length = course_detail[0].text
             number_of_lectures = course_detail[1].text
-            difficulity = course_detail[2].text
+            difficulity = course_detail[2].text         
 
             course_rows.append(
                 [course_url, course_title, course_headline, author, course_rating, number_of_ratings, course_length, number_of_lectures, difficulity]               
@@ -92,6 +111,7 @@ for page_number in range(1,2):
 
 
 print(course_rows)
+print(type(course_rows))
 
 
 driver.quit
